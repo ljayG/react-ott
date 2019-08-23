f616869d8fd8d1f049280bf81a5334b89d608729
ljayG <moguri61@gmail.com>
2019-08-23 15:45:18 +0900

    생활코딩 11.2 컴포넌트 만들기 까지 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello, React!!
-    </div>
-  );
+class Subject extends Component {
+    render() {
+        return (
+            <header>
+                <h1>WEB</h1>
+                world wide web!
+            </header>
+        );
+    }
 }
+
+class Toc extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li><a href="1.html">HTML</a></li>
+                    <li><a href="2.html">CSS</a></li>
+                    <li><a href="3.html">javaScript</a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+class Content extends Component {
+    render() {
+        return (
+            <article>
+                <h2>HTML</h2>
+                HTML is HyperText Markup Language.
+            </article>
+        );
+    }
+}
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Subject></Subject>
+                <Toc></Toc>
+                <Content></Content>
+            </div>
+        );
+    }
+}
+
+
 
 export default App;
