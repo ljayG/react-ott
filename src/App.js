fbade7ccb52d2a5d0d2f874d81bcefe3cfccd91a
ljayG <moguri61@gmail.com>
2019-08-23 17:13:51 +0900

    생활코딩 15.2 State 사용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Content from "./components/Content"
 import './App.css';
 
 class App extends Component {
+    // 어떠한 컴포넌트가 실행될 때, render() 보다 constructor라는 함수가 먼저 실행해서 초기화 담당한다.
+    constructor(props){
+        super(props);
+        this.state = {
+            subject: {title: 'WEB', sub: 'world wide web!'}
+        }
+    }
     render() {
         return (
             <div className="App">
-                <Subject title="WEB" sub="world wide web!"></Subject>
-                <Subject title="React" sub="For ui"></Subject>
+                <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
                 <Toc></Toc>
                 <Content subTitle="HTML" desc="HTML is HyperText Markup Language."></Content>
             </div>
