4c9ec71b0831b4bf7e8bd9819045d61035afdf22
ljayG <moguri61@gmail.com>
2019-08-23 17:57:54 +0900

    생활코딩 state props 그리고 render 교육

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Content extends Component {
     render() {
+        console.log('Content render');
         return (
             <article>
-                <h2>{ this.props.subTitle }</h2>
+                <h2>{ this.props.title }</h2>
                 { this.props.desc }
             </article>
         );
