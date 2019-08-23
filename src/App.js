011d06edb20934461bbbd43d8c0f2a7ffeebcbac
ljayG <moguri61@gmail.com>
2019-08-23 16:14:26 +0900

    생활코딩 14. 컴포넌트 파일 분리하기 완료

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
+import Subject from "./components/Subject"
+import Toc from "./components/Toc"
+import Content from "./components/Content"
 import './App.css';
 
-class Subject extends Component {
-    render() {
-        return (
-            <header>
-                <h1>{ this.props.title }</h1>
-                { this.props.sub }
-            </header>
-        );
-    }
-}
-
-class Toc extends Component {
-    render() {
-        return (
-            <nav>
-                <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">javaScript</a></li>
-                </ul>
-            </nav>
-        );
-    }
-}
-
-class Content extends Component {
-    render() {
-        return (
-            <article>
-                <h2>{ this.props.subTitle }</h2>
-                { this.props.desc }
-            </article>
-        );
-    }
-}
 class App extends Component {
     render() {
         return (
@@ -49,7 +16,5 @@ class App extends Component {
         );
     }
 }
-
-
 
 export default App;
---
+++ src/components/Toc.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Toc extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li><a href="1.html">HTML</a></li>
+                    <li><a href="2.html">CSS</a></li>
+                    <li><a href="3.html">javaScript</a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Toc;
